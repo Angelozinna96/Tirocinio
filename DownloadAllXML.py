2a252adb138471cb992534340d28159e0f5ebb5b
angelo_zinna <angeloshow@github.com>
2018-04-17 17:21:31 +0200

    download completo file xml
    il download e la creazione delle cartelle funziona

--- DownloadAllXML.py
+++ DownloadAllXML.py
@@ -8,16 +8,31 @@ Created on Tue Apr 17 16:03:06 2018
 
 from ftplib import FTP
 import xml.etree.ElementTree as ET
-data_search="20180417"
+import os
+data_search="20180416"
+all_file =[]
+tipologia_file='VIIRS-Image-Bands-SDR-Ellipsoid-Terrain-Corrected-Geo'
+dir_ftp=data_search+'/VIIRS-SDR/'+tipologia_file+'/NPP/'
+
+#connessione ftp
 ftp=FTP('ftp-npp.class.ngdc.noaa.gov')
 ftp.login()
-all_file =[]
-
-ftp.cwd(data_search+'/VIIRS-SDR/VIIRS-Image-Bands-SDR-Ellipsoid-Terrain-Corrected-Geo/NPP/')
+ftp.cwd(dir_ftp)
+#download della lista dei file nella directory dir_ftp
 res=ftp.retrlines('NLST',all_file.append)
 print res
+#dir di base dove salvare i file
+directory_base='/Users/angelozinna/Desktop/università/3anno/tirocinio/dati/python/'
+#creazione cartella della data
+dir_finale=directory_base+data_search+'/'+tipologia_file+"/xml"
+if not os.path.exists(dir_finale):
+    os.makedirs(dir_finale)
+dir_finale=dir_finale+'/'
+#ricerca e download dei file xml
 for name in all_file:
-    if name.find("xml"):
-        ftp.retrbinary('RETR README', open('/Users/angelozinna/Desktop/università/3anno/tirocinio/dati/python', 'wb').write)
+    if name.find("xml")!=-1:
+        new_file=open(dir_finale+name, 'wb')
+        ftp.retrbinary('RETR '+name, new_file.write)
+        new_file.close()
 
 ftp.quit()
