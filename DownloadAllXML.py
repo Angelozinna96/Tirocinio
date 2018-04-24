d512fe1b76bd1c7bca78759c19626df951066063
angelo_zinna <angeloshow@github.com>
2018-04-24 09:50:31 +0200

    prova classse ftp per download di grandi file

--- DownloadAllXML.py
+++ DownloadAllXML.py
@@ -5,7 +5,6 @@ Created on Tue Apr 17 16:03:06 2018
 
 @author: angelozinna
 """
-
 from ftplib import FTP
 import os
 data_search="20180420"
@@ -23,7 +22,7 @@ ftp.cwd(dir_ftp)
 res_ftp=ftp.retrlines('NLST',all_file.append)
 ftp.retrlines('MLSD',all_file_descr.append)
 print res_ftp
-print all_file
+print all_file_descr
 
 res_ftp=res_ftp.split(' ')
 num_el_xml=int(res_ftp[1])/2
@@ -31,17 +30,20 @@ print "num_file xml=%d"% num_el_xml
 
 #dir di base dove salvare i file
 dir_base='/Users/angelozinna/Desktop/università/3anno/tirocinio/dati/python/'
-#creazione cartella della data
+#creazione cartelle della data , xml e h5
 dir_finale=dir_base+data_search+'/'+tipologia_file+"/xml"
+dir_finale_h5=dir_base+data_search+'/'+tipologia_file+"/h5"
 if not os.path.exists(dir_finale):
     os.makedirs(dir_finale)
+if not os.path.exists(dir_finale_h5):
+    os.makedirs(dir_finale_h5)
 dir_finale=dir_finale+'/'
+dir_finale_h5+='/'
 
 
 
 #ricerca e download dei file xml
 import os       
-
 try:
     for name in all_file:
         if name.find("xml")!=-1:
@@ -58,26 +60,38 @@ except:
 #prelevamento info dall'xml
     
 import xml.etree.ElementTree as ET 
-id_buoni=[]
-for name in all_file:
+tar_buoni=[]
+i_buoni=[]
+for i,name in enumerate(all_file):
     if name.find("xml")!=-1:
         tree = ET.parse(dir_finale+name)
         root = tree.getroot()
         #num di granuli presenti nel tar
         num_granuli=int(root.findall('TarFileCount')[0].text)
-        for i,dataset in enumerate(root.findall('Dataset')):
+        for dataset in root.findall('Dataset'):
             nomefile=(dataset.find('FileName').text)
             #visualizzazione orario
-            ore_inizio=nomefile[i].split('_')[3][1:5]
-            ore_fine=nomefile[i].split('_')[4][1:5]
+            ore_inizio=nomefile.split('_')[3][1:5]
+            ore_fine=nomefile.split('_')[4][1:5]
             #ricerca per orario dei granuli
-            if ore_inizio[:2]=="12":
+            if ore_inizio[:4]=="1200":
                 #salvataggio degli id buoni da utilizzare per scaricare i rispettivi file tar
-                id_buoni.append(name.split('.')[0].split('_')[3])
+                #id_buoni.append(name.split('.')[0].split('_')[3])
+                tar_buoni.append(name.split(".manifest")[0])
+                i_buoni.append(i)
                 print ore_inizio, ore_fine   
-id_buoni=list(set(id_buoni))
-
-
-                
-                
-                
+#eliminazione degli id ripetuti
+tar_buoni=list(set(tar_buoni))
+i_buoni=list(set(i_buoni))
+#ricerca dei file tar da scaricare e li scarica, si puo semplificare il processo unendo questa parte a quella di sopra
+from pyftpclient import PyFTPclient
+client_ftp=PyFTPclient('ftp-npp.class.ngdc.noaa.gov',21,'','')
+client_ftp.DownloadFile(dir_ftp+tar_buoni[0],dir_finale_h5+tar_buoni[0])
+'''
+for tar in tar_buoni:
+    new_fileh5=open(dir_finale_h5+tar,'wb')
+    #ftp.retrbinary('RETR '+name,new_fileh5.write)
+    new_fileh5.close()
+    print "download completato"
+ '''
+          
